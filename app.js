28016093e3b71b3db5484666e148697985f32283
Mechatron3000 <simas.basinskas@gmail.com>
2017-07-27 13:31:04 +0300

    1.0.0

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = app.listen(12345);
+var server = app.listen(8080);
 //app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 var socket = require('socket.io');
