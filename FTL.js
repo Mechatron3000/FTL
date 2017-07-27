b9e33270a79ec02af76a50ac182a7bcfa1db29df
Mechatron3000 <simas.basinskas@gmail.com>
2017-07-27 12:57:35 +0300

    1.0

---
+++ FTL.js
@@ -0,0 +1,165 @@
+var express = require('express');
+var app = express();
+var server = app.listen(12345);
+app.use(express.static('public'));
+var socket = require('socket.io');
+var io = socket(server);
+io.sockets.on('connection', newConnection);
+
+function player(id, x, y) {
+	this.id = id;
+	this.x = x;
+	this.y = y;
+	this.angle = 0;
+	this.speed = 0;
+	this.W = false;
+	this.S = false;
+	this.type = 0;
+	this.Weapon = [1, 0, 0, 0];
+	this.hull = 100;
+	this.fuel = 100;
+	this.shield = 1;
+}
+
+function beacon(x, y) {
+	this.x = x;
+	this.y = y;
+	this.type = 0;
+}
+
+var beaconAmount = 100;
+var mapSize = 10000;
+var a = 0.05;
+var maxv = 20;
+var serverTickrate = 1000 / 64;
+
+var setup = false;
+var lastCl;
+var players = [];
+var beacons = [];
+
+setInterval(mainLoop, serverTickrate);
+
+function newConnection(socket) {
+	var ok = false;
+	var x = 0;
+	var y = 0;
+	while (!ok) {
+		ok = true;
+		x = Math.floor(Math.random() * (mapSize + 1));
+		y = Math.floor(Math.random() * (mapSize + 1));
+		for (i = 0; i < beacons.length; i++) {
+			if (500 > Math.sqrt(Math.pow(x - beacons[i].x, 2) + Math.pow(y - beacons[i].y, 2))) {
+				ok = false;
+			}
+		}
+		for (i = 0; i < players.length; i++) {
+			if (500 > Math.sqrt(Math.pow(x - players[i].x, 2) + Math.pow(y - players[i].y, 2))) {
+				ok = false;
+			}
+		}
+	}
+	var Player = new player(socket.id, x, y);
+	players.push(Player);
+	/////////////////////////////////////////////
+	var data = {
+		ID: socket.id,
+		PL: players
+	}
+	socket.emit('setupData', data);
+	/////////////////////////////////////////////
+	socket.on('disconnect', removePlayer);
+	function removePlayer(){
+		for (i = 0; i < players.length; i++) {
+			if (players[i].id == socket.id) {
+				players.splice(i, 1);
+			}
+		}
+	}
+	/////////////////////////////////////////////
+	socket.on('playerUpdate', gotData);
+	function gotData(data) {
+		for (i = 0; i < players.length; i++) {
+			if (players[i].id == socket.id) {
+				players[i].W = data.W;
+				players[i].S = data.S;
+				players[i].angle = data.a;
+				for (j = 0; j < players.length; j++) {
+					if (data.W) {
+						players[i].speed += a;
+						if (players[i].speed > maxv) {
+							players[i].speed = maxv;
+						}
+					}
+					else if (data.S) {
+						players[i].speed -= a;
+						if (players[i].speed < -maxv) {
+							players[i].speed = -maxv;
+						}
+					}
+					else {
+						if (players[i].speed > 0) {
+							players[i].speed -= a;
+						}
+						if (players[i].speed < 0) {
+							players[i].speed += a;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+function mainLoop() {
+	if (setup == false) {
+		generate();
+		setup = true;
+	}
+	if (lastCl != players.length) {
+		console.log(players.length);
+		lastCl = players.length;
+	}
+	for (i = 0; i < players.length; i++) {
+		var x1 = players[i].x + (Math.cos(players[i].angle) * players[i].speed);
+		var y1 = players[i].y - (Math.sin(players[i].angle) * players[i].speed);
+		for (j = 0; j < players.length; j++) {
+			if (i != j) {
+				var x2 = players[j].x + (Math.cos(players[j].angle) * players[j].speed);
+				var y2 = players[j].y - (Math.sin(players[j].angle) * players[j].speed);
+				if (500 < Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))) {
+					players[i].x += (Math.cos(players[i].angle) * players[i].speed);
+					players[i].y -= (Math.sin(players[i].angle) * players[i].speed);
+				}
+				else {
+					players[i].speed = 0;
+				}
+			}
+		}
+	}
+	var data = {
+		PL: players,
+		BC: beacons
+	}
+	io.sockets.emit('update', data);
+}
+
+function generate() {
+	for (i = 0; i < beaconAmount; i++) {
+		var x = 0;
+		var y = 0;
+		var ok = false;
+		while (!ok) {
+			ok = true;
+			var x = Math.floor(Math.random() * (mapSize + 1));
+			var y = Math.floor(Math.random() * (mapSize + 1));
+			for (j = 0; j < beacons.length; j++) {
+				if (500 > Math.sqrt(Math.pow(x - beacons[j].x, 2) + Math.pow(y - beacons[j].y, 2))) {
+					ok = false;
+				}
+			}
+		}
+		var Beacon = new beacon(x, y);
+		beacons.push(Beacon);
+	}
+}
---
+++ public/client.js
@@ -0,0 +1,137 @@
+var socket;
+
+var OX = 0;
+var OY = 0;
+var players = [];
+var beacons = [];
+var ID = ' ';
+
+function setup() {
+  canvasSetup();
+	socket = io.connect('http://78.62.26.153:12345');
+	//socket = io.connect('http://localhost:12345');
+	socket.on('setupData', saveData);
+	socket.on('update', serverUpdate);
+	rectMode(CENTER);
+	//noStroke();
+}
+
+function saveData(data) {
+	ID = data.ID;
+	players = data.PL;
+}
+
+window.onresize = function(event) {
+	canvasSetup();
+}
+
+function canvasSetup() {
+	createCanvas(innerWidth, innerHeight);
+	//grass = loadImage("assets/grass.jpg");
+}
+
+function serverUpdate(data) {
+	players = data.PL;
+	beacons = data.BC;
+	for (i = 0; i < players.length; i++) {
+		if (players[i].id == ID) {
+			OX = players[i].x;
+			OY = players[i].y;
+		}
+	}
+}
+
+function draw() {
+	var data = {
+		W: false,
+		S: false,
+		A: false,
+		D: false,
+		SPACE: false,
+		a: 0
+	}
+	data.d = dist(width / 2, height / 2, mouseX, mouseY);
+	if (mouseX > width / 2 && mouseY < height / 2) {
+		data.a = asin(((height/ 2) - mouseY) / data.d);
+	}
+	if (mouseX < width / 2 && mouseY < height / 2) {
+		data.a = acos(((height/ 2) - mouseY) / data.d) + HALF_PI;
+	}
+	if (mouseX < width / 2 && mouseY > height / 2) {
+		data.a = acos(((width/ 2) - mouseX) / data.d) + PI;
+	}
+	if (mouseX > width / 2 && mouseY > height / 2) {
+		data.a = acos(((width/ 2) - mouseX) / data.d) + PI;
+	}
+	//console.log(data.a);
+//	if (keyIsDown(SHIFT)) {
+//		data.SHIFT = true;
+//	}
+	if (keyIsDown(32)) {
+		data.SPACE = true;
+	}
+	if (keyIsDown(87)) {
+		data.W = true;
+	}
+	if (keyIsDown(83)) {
+		data.S = true;
+	}
+	if (keyIsDown(65)) {
+		data.A = true;
+	}
+	if (keyIsDown(68)) {
+		data.D = true;
+	}
+	socket.emit('playerUpdate', data);
+	clear();
+	background(0);
+	//translate(OX + (width / 2), OY + (height / 2));
+	for (i = 0; i < players.length; i++) {
+		push();
+		translate(players[i].x - OX + (width / 2), players[i].y - OY + (height / 2));
+		rotate(-players[i].angle);
+		if (players[i].type == 0) {
+			fill(150);
+			stroke(0);
+			ellipse(0, 0, 200, 50);
+			translate(100, 0);
+			ellipse(0, 0, 200, 200);
+			ellipse(0, 0, 100, 100);
+			ellipse(0, 0, 50, 40);
+			fill(204, 182, 108);
+			ellipse(0, 0, 20, 20);
+			fill(200);
+			quad(-150, 10, -125, 15, -175, 50, -200, 50);
+			quad(-150, -10, -125, -15, -175, -50, -200, -50);
+			fill(122, 228, 249, 200);
+			if (players[i].W) {
+				ellipse(-300, 50, 30, 15);
+				ellipse(-300, -50, 30, 15);
+			}
+			else if (players[i].S) {
+				ellipse(-100, 50, 30, 15);
+				ellipse(-100, -50, 30, 15);
+			}
+			fill(200);
+			ellipse(-200, 50, 200, 20);
+			ellipse(-200, -50, 200, 20);
+			translate(-100, 0);
+		}
+		//for (j = 0; j < players[i].shield; j++) {
+		//	fill(22, 228, 249, 50);
+		//	ellipse(0, 0, 500, 250);
+		//}
+		noFill();
+		stroke(255, 0, 0);
+		ellipse(0, 0, 500, 500);
+		pop();
+	}
+	for (i = 0; i < beacons.length; i++) {
+		push();
+		translate(beacons[i].x - OX + (width / 2), beacons[i].y - OY + (height / 2));
+		noFill();
+		stroke(255, 0, 0);
+		ellipse(0, 0, 500, 500);
+		pop();
+	}
+}
